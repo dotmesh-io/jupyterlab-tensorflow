5bcc19a04e4ec9f548253b787b5cf62f7f98252c
Kai Davenport <kaiyadavenport@gmail.com>
2018-11-21 11:13:56 +0000

    FIX: use PWD as the notebook dir unless NOTEBOOK_DIR var is specified

--- scripts/start-jupyter.sh
+++ scripts/start-jupyter.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -xe
 
+# make sure we use the -w flag passed to docker run as where to load the notebooks
+# from - this will be the same place as the workspace dot is mounted
+export NOTEBOOK_DIR=${NOTEBOOK_DIR:="$PWD"}
+
 # expects git clone https://github.com/dotmesh-io/jupyterlab-plugin /root/jupyterlab-plugin
 # in dev mode - we have mounted the jupyterlab-plugin repo to /root/jupyterlab-plugin
 source activate base
-jupyter lab --ip 0.0.0.0 --port 8888 --allow-root --notebook-dir /home/jovyan
+jupyter lab --ip 0.0.0.0 --port 8888 --allow-root --notebook-dir "$NOTEBOOK_DIR"
