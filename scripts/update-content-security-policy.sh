091095084978b6423aaea4f49370e788bf00eac9
Luke Marsden <me@lukemarsden.net>
2018-12-16 15:37:38 +0000

    FIX: hmm, I found something that looked like this, so I copied it.

--- scripts/update-content-security-policy.sh
+++ scripts/update-content-security-policy.sh
@@ -4,11 +4,17 @@ set -xe
 # Enable a more liberal Content-Security-Policy so that we can display Jupyter
 # in an iframe.
 
-mkdir -p /usr/local/etc/jupyter
-cat << EOF >> /usr/local/etc/jupyter/jupyter_notebook_config.py
-c.NotebookApp.tornado_settings = {
-  'headers': {
-    'Content-Security-Policy': "frame-ancestors 'self' *"
+jupyter --paths
+
+mkdir -p /usr/local/etc/jupyter/jupyter_notebook_config.d
+cat << EOF >> /usr/local/etc/jupyter/jupyter_notebook_config.d/liberal-csp.json
+{
+  "NotebookApp": {
+    "tornado_settings": {
+      "headers": {
+        "Content-Security-Policy": "frame-ancestors 'self' *"
+      }
+    }
   }
 }
 EOF
