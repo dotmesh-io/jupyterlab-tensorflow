c81698b27883d70d6e97728a7f753328175d5272
Luke Marsden <me@lukemarsden.net>
2018-12-16 14:26:17 +0000

    FIX: use global configuration location per https://jupyterhub.readthedocs.io/en/stable/reference/config-user-env.html as config wasn't being noticed where it was

--- scripts/update-content-security-policy.sh
+++ scripts/update-content-security-policy.sh
@@ -4,7 +4,8 @@ set -xe
 # Enable a more liberal Content-Security-Policy so that we can display Jupyter
 # in an iframe.
 
-cat << EOF >> /usr/etc/jupyter/jupyter_notebook_config.py
+mkdir -p /etc/jupyter
+cat << EOF >> /etc/jupyter/jupyter_notebook_config.py
 c.NotebookApp.tornado_settings = {
   'headers': {
     'Content-Security-Policy': "frame-ancestors 'self' *"
