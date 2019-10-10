f84e822c3720f2d29d974148e3c131918a2a83fa
Itamar Turner-Trauring <itamar@itamarst.org>
2019-10-10 13:51:22 -0400

    Support the GATEWAY_URL_PREFIX env variable.

--- scripts/start-jupyter.sh
+++ scripts/start-jupyter.sh
@@ -6,6 +6,14 @@ set -xeuo pipefail
 export NOTEBOOK_DIR=${NOTEBOOK_DIR:="$PWD"}
 export SHELL=bash
 
+# If GATEWAY_URL_PREFIX is set, run with given URL prefix (see https://jupyter-notebook.readthedocs.io/en/stable/config.html for details):
+if [ "${GATEWAY_URL_PREFIX:-}" != "" ]; then
+    cat > ~/.jupyter/jupyter_notebook_config.py << EOF
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.base_url = "$GATEWAY_URL_PREFIX"
+EOF
+fi
+
 # expects git clone https://github.com/dotmesh-io/jupyterlab-plugin /plugin/jupyterlab-plugin
 # in dev mode - we have mounted the jupyterlab-plugin repo to /plugin/jupyterlab-plugin
 #source activate base
